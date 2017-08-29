8aa9be8d22274103dda21466167729696f5711f4
joetechem <joe@techemstudios.com>
2017-08-29 16:13:02 -0400

    rearranging and cleaning up

--- use_case_scenario/specific_county_search.py
+++ use_case_scenario/specific_county_search.py
@@ -5,39 +5,17 @@ import json
 """
 API to get specific data from wva-dev.sosdata.org
 
-County, Albemarle.
+Testing a simple use-case.
+
+Trying out different api calls: first, at top of parent (or lowest child?);
+                                Second, then more sepcific calls.
+
+User searches for how many:
+    1 - Counties
+    2 - Streams in a specific county ("Albemarle")
+    3 - Sites in that county
 
 """
-
-# call to county:albemarle:sites data
-sites_url = 'https://wva-dev.sosdata.org/sites.json?association=sites&parent_scaffold=counties&county_id=2'
-r3 = requests.get(sites_url)
-
-# test if call successful
-if r3.status_code == 200:
-    print("\tStatus code: 200, API call successful")
-else:
-    print("\tAPI call failed")
-
-sites_list = r3.json()
-
-print("\nTotal number of logged sites in Albemarle: " + str(len(sites_list)))
-
-
-# call to county:albemarle:streams data
-streams_url = 'https://wva-dev.sosdata.org/streams.json?association=streams&parent_scaffold=counties&county_id=2'
-r2 = requests.get(streams_url)
-
-# test if call successful
-if r2.status_code == 200:
-    print("\tStatus code: 200, API call successful")
-else:
-    print("API call failed")
-    
-streams_list = r2.json()
-
-print("\nTotal number of streams in Albermarle: " + str(len(streams_list)))
-
 
 # call to counties data
 url = 'https://wva-dev.sosdata.org/counties.json'
@@ -49,7 +27,7 @@ r = requests.get(url)
 
 # test if call successful
 if r.status_code == 200:
-    print("\tStatus code: 200, API call successful")
+    print("Status code: 200, API call successful")
 else:
     print("API call failed")
 
@@ -60,3 +38,38 @@ else:
 counties_list = r.json()
 
 print("\nTotal number of vasos counties: " + str(len(counties_list)))
+
+
+# call to county:albemarle:streams data
+streams_url = 'https://wva-dev.sosdata.org/streams.json?association=streams&parent_scaffold=counties&county_id=2'
+r2 = requests.get(streams_url)
+
+# test if call successful
+if r2.status_code == 200:
+    print("Status code: 200, API call successful")
+else:
+    print("API call failed")
+    
+streams_list = r2.json()
+
+print("\nTotal number of streams in Albermarle: " + str(len(streams_list)))
+
+
+# call to county:albemarle:sites data
+sites_url = 'https://wva-dev.sosdata.org/sites.json?association=sites&parent_scaffold=counties&county_id=2'
+r3 = requests.get(sites_url)
+
+# test if call successful
+if r3.status_code == 200:
+    print("Status code: 200, API call successful")
+else:
+    print("\tAPI call failed")
+
+sites_list = r3.json()
+
+print("\nTotal number of logged sites in Albemarle: " + str(len(sites_list)))
+
+
+
+
+
