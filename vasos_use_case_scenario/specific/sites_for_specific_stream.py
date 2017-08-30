583570d862353e6a413b3754aac36089e6ba73c7
joetechem <joe@techemstudios.com>
2017-08-30 16:33:06 -0400

    trying list comprehension to fix

--- vasos_use_case_scenario/specific/sites_for_specific_stream.py
+++ vasos_use_case_scenario/specific/sites_for_specific_stream.py
@@ -1,15 +1,13 @@
 """
 Use API to find sites for specific stream
 
-"Show me all sites for this stream"
-
-
 1 - streams in county 
 1a - sites in county
 
     2 - sites for a stream
 
-       3 - site data for that stream 
+       3 - site data for that stream
+       3a - data for one certain site
 
 """
 
@@ -46,9 +44,9 @@ def all_site_data():
 #site_data()
 
 
-######### 2 ######## site data for that stream
+######### 3 ######## site data for that stream
 # This time testing with user input
-user_says = raw_input("Type 'y' to show site data for Buck Mountain Creek: ")
+user_says = raw_input("\nType 'y' to show site data for Buck Mountain Creek: ")
 if user_says == 'y':
     def buck_site_data():
         print "\nShowing list of site data for Buck Mountain Creek, sorted by station identifier:"
@@ -57,6 +55,7 @@ if user_says == 'y':
 buck_site_data()
 
 
+######### 3a ######## site data for that stream
     # 8/30 currently returns same as above. taking a break.
     # Probably need to call to more nested instead of same data as in 2, above
 
@@ -71,6 +70,7 @@ buck_site_data()
 ##        for id, station, metric, eco in sorted([(d['id'],d['station_identifier'],d['multi_metric_score'],d['ecological_conditions']) for d in buck_mtn_creek], key=lambda t:t[1]):
 ##            print'{}: {}'.format(id,station,metric,eco)
 ##        active = False
+
 ##    if user_says_2 == '2'
         
 
